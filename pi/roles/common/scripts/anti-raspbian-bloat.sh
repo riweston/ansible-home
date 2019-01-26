2d6e0e0305a22c5eb2aca6993cbe4fb38f368de7
Richard Weston <richard.weston@cloudreach.com>
2019-01-26 22:25:38 +0000

    Broke out into a list

--- pi/roles/common/scripts/anti-raspbian-bloat.sh
+++ pi/roles/common/scripts/anti-raspbian-bloat.sh
@@ -1,22 +1,39 @@
 # Edu-related packages
 pkgs = (
-    idle python3-pygame python-pygame python-tk
-    idle3 python3-tk
-    python3-rpi.gpio
-    python-serial python3-serial
-    python-picamera python3-picamera
-    python3-pygame python-pygame python-tk
+    idle
+    python3-pygame
+    python-pygame
+    python-tk
+    idle3
     python3-tk
-    debian-reference-en dillo x2x
-    scratch nuscratch
+    python3-rpi.gpio
+    python-serial
+    python3-serial
+    python-picamera
+    python3-picamera
+    python3-pygame
+    python-pygame
+    python-tk
+    python3-tk
+    debian-reference-en
+    dillo
+    x2x
+    scratch
+    nuscratch
     timidity
-    smartsim penguinspuzzle
+    smartsim
+    penguinspuzzle
     pistore
     sonic-pi
     python3-numpy
-    python3-pifacecommon python3-pifacedigitalio python3-pifacedigital-scratch-handler python-pifacecommon python-pifacedigitalio
+    python3-pifacecommon
+    python3-pifacedigitalio
+    python3-pifacedigital-scratch-handler
+    python-pifacecommon
+    python-pifacedigitalio
     oracle-java8-jdk
-    minecraft-pi python-minecraftpi
+    minecraft-pi
+    python-minecraftpi
     wolfram-engine
 )
 
